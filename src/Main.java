fe03fda1dbd67b79a6b52e802c2e4b8a6c3dd1da
Romzes <romzesvxo@mail.ru>
2023-10-22 15:09:13 +0300

    dev

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,20 @@
 import java.io.File;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
-        String folderPath = "C:\\Users\\Romzes\\Desktop\\JVM";
+        String folderPath = "C:\\Users\\Romzes\\Desktop";
         File file = new File(folderPath);
 
-        System.out.println(getFolderSize(file));
+        long start = System.currentTimeMillis();
+        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        ForkJoinPool pool = new ForkJoinPool();
+        long size = pool.invoke(calculator);
+        System.out.println(size);
+
+        long end = System.currentTimeMillis() - start;
+        System.out.println(end);
+//        System.out.println(getFolderSize(file));
     }
 
     public static long getFolderSize(File folder) {
