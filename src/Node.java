3535f191c3343ff836011604d60bef691b75f487
Romzes <romzesvxo@mail.ru>
2023-10-23 18:53:12 +0300

    dev

--- src/Node.java
+++ src/Node.java
@@ -5,6 +5,7 @@ public class Node {
     private File folder;
     private ArrayList<Node> children;
     private long size;
+    private int level;
 
     public Node(File folder) {
         this.folder = folder;
@@ -24,10 +25,27 @@ public class Node {
     }
 
     public void addChild(Node node) {
+        node.setLevel(level + 1);
         children.add(node);
+    }
+
+    private void setLevel(int level) {
+        this.level = level;
     }
 
     public ArrayList<Node> getChildren() {
         return children;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        String size = SizeCalculator.getHumanReadableSize(getSize());
+        builder.append(folder.getName() + " - " + size + "\n");
+        for(Node child : children){
+            builder.append("  " + child.toString());
+        }
+        return builder.toString();
+
     }
 }
