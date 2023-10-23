6bbcf47eb64629ce9d0dbce3e235a1314b11c40f
Romzes <romzesvxo@mail.ru>
2023-10-23 12:36:56 +0300

    dev

---
+++ src/Node.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.util.ArrayList;
+
+public class Node {
+    private File folder;
+    private ArrayList<Node> children;
+    private long size;
+
+    public Node(File folder) {
+        this.folder = folder;
+        children = new ArrayList<>();
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+
+    public File getFolder() {
+        return folder;
+    }
+
+    public void addChild(Node node) {
+        children.add(node);
+    }
+
+    public ArrayList<Node> getChildren() {
+        return children;
+    }
+}
