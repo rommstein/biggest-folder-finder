ee81b6cb18be26f6fe8b6929889f8c96c10c2964
Romzes <romzesvxo@mail.ru>
2023-10-27 15:50:08 +0300

    dev

---
+++ src/ParametersBag.java
@@ -0,0 +1,11 @@
+public class ParametersBag {
+    public ParametersBag(String args[]){
+
+    }
+    public long getLimit(){
+        return 0;
+    }
+    public String getPath(){
+        return "";
+    }
+}
